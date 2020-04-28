aecb9debf78faa4e87f5732463a646a0b0bd5e37
Md. Rafiul Alam <rafiul.corp@akij.net>
2020-04-28 13:07:59 +0600

    update style

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,7 +14,19 @@
 			
 			g.preventDefault();
 		} );
+
+
+	
+
+
 })(jQuery); 
 
  
-    
+function myFunction() {
+	var x = document.getElementById("myLinks");
+	if (x.style.display === "block") {
+	  x.style.display = "none";
+	} else {
+	  x.style.display = "block";
+	}
+  }
